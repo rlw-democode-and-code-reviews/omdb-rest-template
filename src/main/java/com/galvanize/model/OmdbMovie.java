fe278c3a7d76ec1399988b78b475d8596acd0fb4
Robert Wing <robertwing@Robs-Mac-mini.local>
2020-04-06 15:09:08 -0700

    initial commit

---
+++ src/main/java/com/galvanize/model/OmdbMovie.java
@@ -0,0 +1,91 @@
+package com.galvanize.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OmdbMovie {
+    @JsonProperty("Title")
+    private String title;
+    @JsonProperty("Year")
+    private String year;
+    @JsonProperty("Rated")
+    private String rated;
+    @JsonProperty("Released")
+    String released;
+    @JsonProperty("Runtime")
+    private String runTime;
+    @JsonProperty("Genre")
+    private String genre;
+    @JsonProperty("Director")
+    private String director;
+    @JsonProperty("Writer")
+    private String writer;
+    @JsonProperty("Actors")
+    private String actors;
+    @JsonProperty("Plot")
+    private String plot;
+    @JsonProperty("Language")
+    private String language;
+    @JsonProperty("Country")
+    private String country;
+    @JsonProperty("Awards")
+    private String awards;
+    @JsonProperty("Poster")
+    private String poster;
+    @JsonProperty("Ratings")
+    List<OmdbRating> ratings;
+    @JsonProperty("Metascore")
+    private String metascore;
+    @JsonProperty("imdbRating")
+    private String imdbRating;
+    @JsonProperty("imdbVotes")
+    private String imdbVotes;
+    @JsonProperty("imdbID")
+    private String imdbId;
+    @JsonProperty("Type")
+    private String type;
+    @JsonProperty("DVD")
+    private String dvd;
+    @JsonProperty("BoxOffice")
+    private String boxOffice;
+    @JsonProperty("Production")
+    private String production;
+    @JsonProperty("Website")
+    private String website;
+    @JsonProperty("Response")
+    private String response;
+
+    class OmdbRating {
+        @JsonProperty("Source")
+        private String source;
+        @JsonProperty("Value")
+        private String value;
+
+        public String getSource() {
+            return source;
+        }
+
+        public void setSource(String source) {
+            this.source = source;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "OmdbRating{" +
+                    "source='" + source + '\'' +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
+    }
+}
---
+++ src/main/java/com/galvanize/model/SearchResults.java
@@ -0,0 +1,87 @@
+package com.galvanize.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.xml.transform.Result;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SearchResults {
+
+    @JsonProperty("search")
+    private List<Result> search;
+
+    public List<Result> getSearch() {
+        return search;
+    }
+
+    public void setSearch(List<Result> search) {
+        this.search = search;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    class result {
+        /*
+       {
+               "Title": "Birds of a Feather",
+               "Year": "2000",
+               "imdbID": "tt0413838",
+               "Type": "movie",
+               "Poster": "N/A"
+        }
+        */
+        @JsonProperty("Title")
+        private String title;
+        @JsonProperty("Year")
+        private String year;
+        @JsonProperty("imdbID")
+        private String imdbId;
+        @JsonProperty("Type")
+        private String type;
+        @JsonProperty("Poster")
+        private String poster;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getYear() {
+            return year;
+        }
+
+        public void setYear(String year) {
+            this.year = year;
+        }
+
+        public String getImdbId() {
+            return imdbId;
+        }
+
+        public void setImdbId(String imdbId) {
+            this.imdbId = imdbId;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getPoster() {
+            return poster;
+        }
+
+        public void setPoster(String poster) {
+            this.poster = poster;
+        }
+    }
+
+}
+

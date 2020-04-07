1f6300d6bcaf74a63eb1b7ced4d6f302e7a7b82b
rlwing <rob@wingjr.com>
2020-04-06 20:18:53 -0700

    caps

--- src/main/java/com/galvanize/model/SearchResults.java
+++ src/main/java/com/galvanize/model/SearchResults.java
@@ -8,7 +8,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchResults {
 
-    @JsonProperty("search")
+    @JsonProperty("Search")
     private List<Result> search;
     private int totalResults;
     boolean response;
